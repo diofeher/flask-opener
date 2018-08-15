4d68c23670a3c4f835e2b102c85f1d7f61179fbd
Diógenes Augusto Fernandes Hermínio <diofeher@gmail.com>
2018-08-15 09:53:30 -0300

    Fix template and bump to 1.0.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-Opener',
-    version='0.1.3',
+    version='0.1.4',
     url='http://github.com/diofeher/flask-opener/',
     license='BSD',
     author='diofeher',
